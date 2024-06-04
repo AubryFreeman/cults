8def3a0d48241b0013ca1c5a8d42c52a9179cdbf
Aubry Freeman <aubryfreeman23@gmail.com>
2024-06-03 19:04:26 -0500

    Added cults fixtures

--- seed_database.sh
+++ seed_database.sh
@@ -5,6 +5,7 @@ rm -rf ./cultsapi/migrations
 python3 manage.py migrate
 python3 manage.py makemigrations cultsapi
 python3 manage.py migrate cultsapi
-python3 manage.py loaddata user
-python3 manage.py loaddata token
+python3 manage.py loaddata users
+python3 manage.py loaddata tokens
+python3 manage.py loaddata cults
 
